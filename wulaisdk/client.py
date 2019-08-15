99a1cde39e1f65fb5b5e5cfc519dded58fd24eb4
yangte <yangte@laiye.com>
2019-08-15 16:59:28 +0800

    add log demo

--- wulaisdk/client.py
+++ wulaisdk/client.py
@@ -1,6 +1,7 @@
 import time
 import uuid
 import hashlib
+import logging
 
 from wulaisdk.http import BaseRequest
 from wulaisdk import http_codes
@@ -8,13 +9,30 @@ from wulaisdk.request import CommonRequest
 from wulaisdk.exceptions import ServerException, ClientException, ERR_INFO
 
 
+DEBUG = False
+
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+logger.addHandler(handler)
+
+
 class WulaiClient:
-    def __init__(self, pubkey, secret, endpoint="https://openapi.wul.ai", api_version="v2"):
+    def __init__(self, pubkey, secret, endpoint="https://openapi.wul.ai", api_version="v2", debug=False):
         self.pubkey = pubkey
         self.secret = secret
         self.endpoint = endpoint
         self.api_version = api_version
         self.check_api_version()
+        global DEBUG
+        DEBUG = debug
+        self.logger_level_init()
+
+    def logger_level_init(self):
+        global logger
+        if DEBUG:
+            logger.setLevel(logging.DEBUG)
+        else:
+            logger.setLevel(logging.ERROR)
 
     def check_api_version(self):
         if self.api_version not in ["v1", "v2"]:
@@ -53,28 +71,28 @@ class WulaiClient:
     def response_wrapper(self, response):
         js = None
         exception = None
-        if response is not None and http_codes.PARAMS_ERROR > response.status_code >= http_codes.OK:
+        if response is not None and response.status_code == http_codes.OK:
             js = response.json() or {}
         elif response is not None and response.status_code >= http_codes.PARAMS_ERROR:
+            try:
+                err_msg = response.json()["message"]
+            except Exception:
+                err_msg = ""
             if response.status_code == 400:
-                try:
-                    err_msg = response.json()["message"]
-                except Exception:
-                    err_msg = ERR_INFO["SDK_INVALID_PARAMS"]
-                exception = ClientException("SDK_INVALID_PARAMS", err_msg)
+                exception = ClientException("SDK_INVALID_PARAMS", err_msg or ERR_INFO["SDK_INVALID_PARAMS"])
             elif response.status_code == 401:
                 exception = ClientException("SDK_INVALID_CREDENTIAL", ERR_INFO["SDK_INVALID_CREDENTIAL"])
             elif response.status_code == 405:
-                exception = ServerException("SDK_METHOD_NOT_ALLOW", ERR_INFO["SDK_METHOD_NOT_ALLOW"])
+                exception = ClientException("SDK_METHOD_NOT_ALLOW", ERR_INFO["SDK_METHOD_NOT_ALLOW"])
             else:
-                try:
-                    err_msg = response.text
-                except Exception:
-                    err_msg = "Sth error"
+                if not err_msg:
+                    try:
+                        err_msg = response.text
+                    except Exception:
+                        err_msg = "Sth error"
                 exception = ServerException("SDK_UNKNOWN_SERVER_ERROR",
                                             err_msg,
                                             response.status_code)
-
         return js, exception
 
     def handle_single_request(self, request):
@@ -83,6 +101,8 @@ class WulaiClient:
         headers = self.get_headers(request)
         method = request.opts.get("method", "POST")
         timeout = request.opts.get("timeout", 3)
+        logger.debug("Request received. Action: {}. Endpoint: {}. Params: {}. Opts: {}".format(
+            request.action, self.endpoint, request.params, request.opts))
 
         r = BaseRequest(self.endpoint)
         if method.upper() == "POST":
@@ -109,9 +129,12 @@ class WulaiClient:
                 retries = -1
             else:
                 retries -= 1
+                logger.debug("Retry needed. Action: {}. Number of remaining retries: {}".format(
+                    request.action, retries))
             if retries < 0:
                 break
             time.sleep(0.1)
         if exception:
             raise exception
+        logger.debug("Response received. Action: {}. Response-body: {}".format(request.action, body))
         return body
